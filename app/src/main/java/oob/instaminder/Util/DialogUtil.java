25935dddccaf911b901a4d2ab4c3954967755921
Julio Lopez <julio.lopez@t-systems.com>
2018-07-31 20:51:39 +0200

    feat: Add negative action text and callback to DialogUtil

--- app/src/main/java/oob/instaminder/Util/DialogUtil.java
+++ app/src/main/java/oob/instaminder/Util/DialogUtil.java
@@ -5,21 +5,6 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 
 public class DialogUtil {
-    public static void showDialog(Context context,
-                                  String title,
-                                  String message,
-                                  String positiveActionText,
-                                  DialogInterface.OnClickListener positiveCallback,
-                                  String negativeActionText,
-                                  DialogInterface.OnClickListener negativeCallback) {
-        new AlertDialog.Builder(context)
-                .setTitle(title)
-                .setMessage(message)
-                .setPositiveButton(positiveActionText, positiveCallback)
-                .setNegativeButton(negativeActionText, negativeCallback)
-                .show();
-    }
-
     public static void showAlertDialog(Context context,
                                        String title,
                                        String message,
@@ -33,10 +18,22 @@ public class DialogUtil {
                                        String positiveActionText,
                                        DialogInterface.OnClickListener positiveCallback,
                                        boolean cancellable) {
+        showAlertDialog(context, title, message, positiveActionText, positiveCallback, null, null, cancellable);
+    }
+
+    public static void showAlertDialog(Context context,
+                                       String title,
+                                       String message,
+                                       String positiveActionText,
+                                       DialogInterface.OnClickListener positiveCallback,
+                                       String negativeActionText,
+                                       DialogInterface.OnClickListener negativeCallback,
+                                       boolean cancellable) {
         new AlertDialog.Builder(context)
                 .setTitle(title)
                 .setMessage(message)
                 .setPositiveButton(positiveActionText, positiveCallback)
+                .setNegativeButton(negativeActionText, negativeCallback)
                 .setCancelable(cancellable)
                 .show();
     }
