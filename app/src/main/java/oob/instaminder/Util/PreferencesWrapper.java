840dc26de9f2ad13bbc63d1942d35e79b22aaf23
Julio Lopez <julio.lopez@t-systems.com>
2018-08-17 15:03:00 +0200

    feat: Add First Launch trigger and Number of ads shown to SharedPreferences

--- app/src/main/java/oob/instaminder/Util/PreferencesWrapper.java
+++ app/src/main/java/oob/instaminder/Util/PreferencesWrapper.java
@@ -6,6 +6,8 @@ import android.util.Base64;
 public class PreferencesWrapper {
     private String PREFERENCES_NICK_KEY = "preferences_nick_key";
     private String PREFERENCES_PASSWORD_KEY = "preferences_password_key";
+    private String PREFERENCES_FIRST_LAUNCH_KEY = "preferences_first_launch_key";
+    private String PREFERENCES_NUMBER_AD_SHOWN = "preferences_number_ad_shown";
 
     private SharedPreferences preferences;
 
@@ -26,6 +28,26 @@ public class PreferencesWrapper {
                 .putString(PREFERENCES_NICK_KEY, this.encode(nick))
                 .putString(PREFERENCES_PASSWORD_KEY, this.encode(password))
                 .commit();
+    }
+
+    public boolean isFirstLaunch() {
+        return this.preferences.getBoolean(PREFERENCES_FIRST_LAUNCH_KEY, true);
+    }
+
+    public void updateFirstLaunch() {
+        this.preferences.edit()
+                .putBoolean(PREFERENCES_FIRST_LAUNCH_KEY, false)
+                .apply();
+    }
+
+    public int getNumberAdShown() {
+        return this.preferences.getInt(PREFERENCES_NUMBER_AD_SHOWN, 0);
+    }
+
+    public void increaseNumberAdShown() {
+        this.preferences.edit()
+                .putInt(PREFERENCES_NUMBER_AD_SHOWN, this.getNumberAdShown() + 1)
+                .apply();
     }
 
     private String encode(String toEncode) {
