1c37a4edd0762b3d0940b6d7f823f607f4c51ea7
Julio Lopez <julio.lopez@t-systems.com>
2018-08-17 15:08:01 +0200

    feat: Inject use cases to Add new Photo screen (DI)

--- app/src/main/java/oob/instaminder/AddPhotoScheduleComponent/Framework/DependencyInjection/AddPhotoScheduleComponentModule.java
+++ app/src/main/java/oob/instaminder/AddPhotoScheduleComponent/Framework/DependencyInjection/AddPhotoScheduleComponentModule.java
@@ -5,10 +5,17 @@ import android.content.Context;
 import dagger.Module;
 import dagger.Provides;
 import io.realm.Realm;
+import oob.instaminder.AddPhotoScheduleComponent.Data.GetNumberAdShownUseCase.GetNumberAdShownUseCaseRepository;
+import oob.instaminder.AddPhotoScheduleComponent.Data.IncreaseNumberAdShownUseCase.IncreaseNumberAdShownUseCaseRepository;
 import oob.instaminder.AddPhotoScheduleComponent.Data.SavePhotoUseCase.SavePhotoUseCaseRepository;
+import oob.instaminder.AddPhotoScheduleComponent.Domain.GetNumberAdShownUseCase.GetNumberAdShownUseCaseRepositoryInterface;
+import oob.instaminder.AddPhotoScheduleComponent.Domain.GetNumberAdShownUseCase.GetNumberAdShownUseCaseViewRepository;
+import oob.instaminder.AddPhotoScheduleComponent.Domain.IncreaseNumberAdShownUseCase.IncreaseNumberAdShownUseCaseRepositoryInterface;
+import oob.instaminder.AddPhotoScheduleComponent.Domain.IncreaseNumberAdShownUseCase.IncreaseNumberAdShownUseCaseViewInterface;
 import oob.instaminder.AddPhotoScheduleComponent.Domain.SavePhotoUseCase.SavePhotoUseCaseRepositoryInterface;
 import oob.instaminder.AddPhotoScheduleComponent.Domain.SavePhotoUseCase.SavePhotoUseCaseViewInterface;
 import oob.instaminder.AddPhotoScheduleComponent.Domain.ViewInterface;
+import oob.instaminder.Util.PreferencesWrapper;
 
 @Module
 public class AddPhotoScheduleComponentModule {
@@ -28,5 +35,29 @@ public class AddPhotoScheduleComponentModule {
     @Provides
     SavePhotoUseCaseRepositoryInterface provideSavePhotoUseCaseRepositoryInterface(Context context, Realm realm) {
         return new SavePhotoUseCaseRepository(context, realm);
+    }
+
+    @AddPhotoScheduleComponentScopeInterface
+    @Provides
+    GetNumberAdShownUseCaseViewRepository provideGetNumberAdShownUseCaseViewRepository() {
+        return this.viewInterface;
+    }
+
+    @AddPhotoScheduleComponentScopeInterface
+    @Provides
+    GetNumberAdShownUseCaseRepositoryInterface provideGetNumberAdShownUseCaseRepositoryInterface(PreferencesWrapper preferencesWrapper) {
+        return new GetNumberAdShownUseCaseRepository(preferencesWrapper);
+    }
+
+    @AddPhotoScheduleComponentScopeInterface
+    @Provides
+    IncreaseNumberAdShownUseCaseViewInterface provideIncreaseNumberAdShownUseCaseViewInterface() {
+        return this.viewInterface;
+    }
+
+    @AddPhotoScheduleComponentScopeInterface
+    @Provides
+    IncreaseNumberAdShownUseCaseRepositoryInterface provideIncreaseNumberAdShownUseCaseRepositoryInterface(PreferencesWrapper preferencesWrapper) {
+        return new IncreaseNumberAdShownUseCaseRepository(preferencesWrapper);
     }
 }
